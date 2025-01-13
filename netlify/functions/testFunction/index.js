270eadaee449e026b7a9be63c3345dba249151e2
Thund4r <chongzhekhang@gmail.com>
2025-01-13 19:42:17 +0800

    Deleted testFunction

--- netlify/functions/testFunction/index.js
+++
@@ -1,8 +0,0 @@
-import { PutCommand } from "@aws-sdk/lib-dynamodb";
-import { ddbDocClient } from "./ddbDocClient.js";
-
-myData.then(function(data){ // .then() tells it to wait until the promise is resolved
-    const pieceOfData = data['whatever'] // and THEN run the function inside
-  })
-
-
