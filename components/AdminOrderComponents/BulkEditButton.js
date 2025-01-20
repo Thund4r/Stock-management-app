b6b5e8854cd34c50291a7a7a1d7a2ae6b33e52c8
Qing-debug <hongjiaqing1234@gmail.com>
2025-01-20 22:03:58 +0800

    feat: added ability to query Orders API to test the GET functionality

--- components/AdminOrderComponents/BulkEditButton.js
+++ components/AdminOrderComponents/BulkEditButton.js
@@ -1,7 +1,30 @@
 export function BulkEditButton(){
+
+    const  sendGetReq = async () =>{
+        try{
+            const response = await fetch("http://localhost:8888/.netlify/functions/orders", {
+                method: "GET",
+            })
+            console.log("Here's the response:", response)
+            console.log("Status code:", response.status)
+            const response_body = await response.json() //gets rid of everything, turns the body property of the response into a JS object (not necessarily a plain object) and returns that.
+            console.log(response_body)
+            console.log("Success:", response_body.success)
+            console.log("Message:", response_body.message)
+            console.log("Error:", response_body.error)
+            // if(response.ok){
+            //     console.log("The response.ok status:", response.ok)
+            //     const response_body = await response.json()
+            // }
+        }catch(error){
+            console.log("An error was thrown,", error)
+        }
+    }
+
+
     return (
         <div>
-            <button>Placeholder</button>
+            <button onClick = {sendGetReq}>Click to test get request</button>
         </div>        
     )
 }
