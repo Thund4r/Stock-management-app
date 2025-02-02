e53bc9e6dc4a84bf1bfbec5ed84b40c9596c3187
Qing-debug <hongjiaqing1234@gmail.com>
2025-02-02 21:35:41 +0800

    feat: add fetching latest data from orderDB for storage in localstorage
    Will be used later on to prevent race conditions
    from occuring by ensuring data retrieved at time
    of edit button being clicked is the same as when
    the changes are made on the database

--- components/AdminOrderComponents/BulkEditButton.js
+++ components/AdminOrderComponents/BulkEditButton.js
@@ -1,30 +1,35 @@
-export function BulkEditButton(){
+import style from "./BulkEditButton.module.css"
 
-    const  sendGetReq = async () =>{
-        try{
-            const response = await fetch("http://localhost:8888/.netlify/functions/orders", {
-                method: "GET",
-            })
-            console.log("Here's the response:", response)
-            console.log("Status code:", response.status)
-            const response_body = await response.json() //gets rid of everything, turns the body property of the response into a JS object (not necessarily a plain object) and returns that.
-            console.log(response_body)
-            console.log("Success:", response_body.success)
-            console.log("Message:", response_body.message)
-            console.log("Error:", response_body.error)
-            // if(response.ok){
-            //     console.log("The response.ok status:", response.ok)
-            //     const response_body = await response.json()
-            // }
-        }catch(error){
-            console.log("An error was thrown,", error)
-        }
+export default function BulkEditButton() {
+  const sendGetReq = async () => {
+    try {
+      const response = await fetch("../../.netlify/functions/orders", {
+        method: "GET",
+      });
+      console.log("Here's the response:", response);
+      console.log("Status code:", response.status);
+      const response_body = await response.json(); //gets rid of everything, turns the body property of the response into a JS object (not necessarily a plain object) and returns that.
+      console.log(response_body);
+      console.log("Success:", response_body.success);
+      console.log("Message:", response_body.message);
+      console.log("Error:", response_body.error);
+      return response_body.items
+    } catch (error) {
+      console.log("An error was thrown,", error);
     }
+  };
 
-
-    return (
-        <div>
-            <button onClick = {sendGetReq}>Click to test get request</button>
-        </div>        
-    )
+  return (
+    <div>
+      <button 
+      className = {style.test}
+        onClick={async () => {
+          const items = await sendGetReq();
+          sessionStorage.setItem("orders", JSON.stringify(items));
+        }}
+      >
+        Click to test get request
+      </button>
+    </div>
+  );
 }
