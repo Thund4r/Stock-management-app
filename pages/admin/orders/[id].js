114222dc4a0bb9eb6a214173e97b95cbb12f8c8e
Thund4r <chongzhekhang@gmail.com>
2025-02-05 18:33:10 +0800

    Temporarily removed contents of [id] so it would stop causing build errors

--- pages/admin/orders/[id].js
+++ pages/admin/orders/[id].js
@@ -1,68 +1,68 @@
-import { revalidateTag } from "next/cache";
+// import { revalidateTag } from "next/cache";
 
 
-const createPagesToRender = (orders) => {
+// const createPagesToRender = (orders) => {
 
-  return (
-    [{params: {id: String(425)}}]
-);
-  // return orders.map(({ orderID }) => {
-  //   return {
-  //     params: {
-  //       id: String(orderID),
-  //     },
-  //   };
-  // });
-};
+//   return (
+//     [{params: {id: String(425)}}]
+// );
+//   // return orders.map(({ orderID }) => {
+//   //   return {
+//   //     params: {
+//   //       id: String(orderID),
+//   //     },
+//   //   };
+//   // });
+// };
 
-export const getStaticPaths = async () => {
-  try {
-    // let response = await fetch(`${process.env.NEXT_PUBLIC_ROOT_PAGE}/.netlify/functions/orders`, {
-    //   method: "GET",
-    // });
-    // response = await response.json();
-    // const orders = response.items;
-    let orders=[]
-    return {
-      paths: createPagesToRender(orders),
-      fallback: 'blocking',
-    };
-  } catch (err) {
-    console.log("Failed to retrieve orders from orderDB during build time");
-    console.log(err);
-  }
-};
+// export const getStaticPaths = async () => {
+//   try {
+//     // let response = await fetch(`${process.env.NEXT_PUBLIC_ROOT_PAGE}/.netlify/functions/orders`, {
+//     //   method: "GET",
+//     // });
+//     // response = await response.json();
+//     // const orders = response.items;
+//     let orders=[]
+//     return {
+//       paths: createPagesToRender(orders),
+//       fallback: 'blocking',
+//     };
+//   } catch (err) {
+//     console.log("Failed to retrieve orders from orderDB during build time");
+//     console.log(err);
+//   }
+// };
 
 
-export const getStaticProps = async ({ params }) => {
-  try{
-    let response = await fetch(`${process.env.NEXT_PUBLIC_ROOT_PAGE}/.netlify/functions/orderID?id=${params.id}`, { method: "GET" });
+// export const getStaticProps = async ({ params }) => {
+//   try{
+//     let response = await fetch(`${process.env.NEXT_PUBLIC_ROOT_PAGE}/.netlify/functions/orderID?id=${params.id}`, { method: "GET" });
   
-    response = await response.json()
-    if(!response.items){
-      return { notFound: true }
-    }
-    return {
-      props: {
-        customerName: String(response.items.customerName),
-        delivDate:  String(response.items.deliveryDate),
-        totalPrice:   String(response.items.totalPrice),
-        outletName:   String(response.items.outletName),
-        delivStatus:  String(response.items.deliveryStatus),
-        orderID:  String(response.items.orderID)
-      },
-      revalidate: 60,
-    };
-  }catch(err){
-    console.log("Error occured in orderID Api", err)
-  } 
-};
+//     response = await response.json()
+//     if(!response.items){
+//       return { notFound: true }
+//     }
+//     return {
+//       props: {
+//         customerName: String(response.items.customerName),
+//         delivDate:  String(response.items.deliveryDate),
+//         totalPrice:   String(response.items.totalPrice),
+//         outletName:   String(response.items.outletName),
+//         delivStatus:  String(response.items.deliveryStatus),
+//         orderID:  String(response.items.orderID)
+//       },
+//       revalidate: 60,
+//     };
+//   }catch(err){
+//     console.log("Error occured in orderID Api", err)
+//   } 
+// };
 
 
 
-export default function page({ customerName, delivDate, totalPrice, outletName, delivStatus, orderID }) {
+export default function page({}) {
   return (
-  <div>information regarding orderID for this page: {`${customerName} ${delivDate} ${totalPrice} ${outletName} ${delivStatus} ${orderID}`}</div>
+  <div>TEMP</div>
 ) 
 
 }
