bc07c183b472571382f3ca87df79c73b3e1e167c
Qing-debug <hongjiaqing1234@gmail.com>
2025-02-23 16:27:42 +0800

    feat: fully implement edit page for orders in data base

--- pages/admin/orders/edit.js
+++ pages/admin/orders/edit.js
@@ -1,37 +1,43 @@
 import { useEffect } from "react";
+import NavBar from "components/NavBar";
+import EditContainer from "@components/AdminOrderComponents/edit/EditContainer";
+import styles from "./edit.module.css";
 
-export const getStaticProps = async () => {
-    let orders;
-    try {
-      let response = await fetch(`${process.env.NEXT_PUBLIC_ROOT_PAGE}/.netlify/functions/orders`, {
-        method: "GET",
-      });
-      console.log("Here's the response:", response)
-      response = await response.json();
-      orders = response.items;
-      console.log(response)
-    } catch (err) {
-      console.log("Failed to retrieve orders from orderDB during build time");
-      console.log(err);
-    }
-  
-    return {
-      props: {
-        orders: orders,
-      },
-    };
+export const getServerSideProps = async () => {
+  let orders;
+  try {
+    let response = await fetch(`${process.env.NEXT_PUBLIC_ROOT_PAGE}/.netlify/functions/orders`, {
+      method: "GET",
+    });
+    response = await response.json();
+    orders = response.items;
+  } catch (err) {
+    console.log(err);
+  }
+
+  return {
+    props: {
+      orders: orders,
+    },
   };
+};
 
+export default function page({ orders }) {
+  useEffect(() => {
+    sessionStorage.setItem("orders", JSON.stringify(orders));
+  }, []);
 
-
-export default function page({ orders }){
-
-    useEffect(() => {
-        sessionStorage.setItem("orders", JSON.stringify(orders));
-      }, []); //empty dependency array so it only runs after initial render and that's it.
-
-      
-    return (
-      <div>hello</div>
-    )
+  return (
+    <div className={styles.pageLayout}>
+      <nav className={styles.leftSideBarNav}>
+        <NavBar/>
+      </nav>
+      <div className={styles.mainContainer}>
+        <main className = {styles.content}>
+          <h4>Bulk edit</h4>
+          <EditContainer orders={orders} />
+        </main>
+      </div>
+    </div>
+  );
 }
