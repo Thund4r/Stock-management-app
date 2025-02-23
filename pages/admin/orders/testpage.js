047cd65908817cf37f98e92d7926c7c593e736e0
Qing-debug <hongjiaqing1234@gmail.com>
2025-02-23 16:49:00 +0800

    feat: test page for testing components

--- pages/admin/orders/testpage.js
+++ pages/admin/orders/testpage.js
@@ -1,16 +1,4 @@
-const paths = ["/admin/orders", "/admin/orders/400", "/admin/orders/401"]
-
-
-export default function page(){
-    return (
-        <button onClick={async () => {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_ROOT_PAGE}/.netlify/functions/revalidations`, {
-                method:"POST",
-                headers: {
-                    'Content-Type': 'application/json',
-                  },
-                body: JSON.stringify(paths)
-            })
-        }}>Click to see if serverless function calls nextJS api properly</button>
-    )
+import AddOrderForm from "@components/AdminOrderComponents/AddOrderForm";
+export default function page() {
+  return (<AddOrderForm></AddOrderForm>);
 }
